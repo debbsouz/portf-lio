5e9ab084c793b92ce202bf9025601401741433b9
Debora <deborasouza.p@outlook.com>
2026-04-12 01:17:41 -0300

    Interação magnética

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ document.addEventListener("mousemove", e => {
   cursor.style.top = e.clientY + "px";
   cursor.style.left = e.clientX + "px";
 });
+
+const cards = document.querySelectorAll(".stack-card");
+
+cards.forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const moveX = (x - rect.width / 2) / 10;
+    const moveY = (y - rect.height / 2) / 10;
+
+    card.style.transform = `translate(${moveX}px, ${moveY}px)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "translate(0,0)";
+  });
+});
